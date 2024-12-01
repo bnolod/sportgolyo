1a5c8f30e409dd494e496e46b78c43fee50ad220
bolond <sadfoolc@gmail.com>
2024-12-01 22:09:37 +0100

    alap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlayersModule } from './players/players.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [],
+  imports: [PlayersModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
